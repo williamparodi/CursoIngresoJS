379ee8e4d78c9ad6f29f0b1ca8cfdadfe01608be
xwillyxgb <jonwilliamparodi@gmail.com>
2021-07-06 01:10:13 -0300

    utlima modificación ejercicios 1 al 8

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -5,5 +5,20 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	alert("ok");
+	var dividendo;
+	var divisor;
+    var resultado;
+
+    dividendo = document.getElementById("txtIdNumeroDividendo").value;
+    divisor = document.getElementById("txtIdNumeroDivisor").value;
+
+    dividendo = parseInt(dividendo);
+    divisor = parseInt(divisor);
+
+    resultado = dividendo / divisor;
+
+    alert("El resto es " + resultado); // El resto es 0?
+
+
+
 }
--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,21 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var sueldo;
+	var aumento;
+	var resultado;
 
-	alert("ok");
+	sueldo = document.getElementById("txtIdSueldo").value;
+    
+    aumento = 1.10;
+
+    sueldo = parseInt(sueldo);
+    aumento = parseFloat(aumento);
+    
+    resultado = sueldo * aumento;
+
+
+
+    
+    document.getElementById("txtIdResultado").value = resultado;							
 }
