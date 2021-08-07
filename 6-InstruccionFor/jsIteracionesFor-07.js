a4dec759577670019dbca810b18bd63ebc05a0a2
xwillyxgb <jonwilliamparodi@gmail.com>
2021-08-07 10:43:49 -0300

    no sirvo para nada

--- 6-InstruccionFor/jsIteracionesFor-07.js
+++ 6-InstruccionFor/jsIteracionesFor-07.js
@@ -1,7 +1,116 @@
+/*
+EJERCICIO 2
+El dueño de una tienda dedicada a la compra/venta de cartas de Yu-Gi-Oh! desea ingresar en el
+sistema las ventas realizadas en el dia de la fecha y conocer ciertos datos en base a las cartas
+que se vendieron.
+Se ingresara hasta que el usuario decida:
+* Nombre de la carta.
+* Tipo de carta: Validar "monstruo", "magica", "trampa".
+* Rareza: Validar "rara", "super rara", "ultra rara".
+* Precio: Validar que no sea 0 o negativo.
+Se pide informar por document.write:
+A) El nombre y rareza de la carta con mayor precio.
+
+B) Cuantas cartas de tipo "trampa" y de rareza "rara o super rara" fueron vendidas.
+
+C) El promedio de precio de las cartas de tipo "monstruo" de rareza "ultra rara" con un precio menor a 500.
+*/
+
+
 function mostrar()
 {
+	var nombre;
+	var tipo;
+	var rareza;
+	var precio;
+	var respuesta;
 
-	alert("ok");
+	var nombreMayorPrecio;
+	var rarezaMayorPrecio;
+	var banderaDelPrimerMaximo;
+	var precioMaximo;
 
+	var contadorTrampaRaraSuper;
+	var contadorMonstruoUltra;
+	var precioMonstruo;
+	var promedioPrecio;
+
+	banderaDelPrimerMaximo = true;
+	respuesta = true;
+	contadorTrampaRaraSuper = 0;
+	contadorMonstruoUltra = 0;
+	precioMonstruo = 0;
+
+	while(respuesta == true)
+	{
+
+		nombre = prompt("Ingrese nombre");
+
+		while(!isNaN(nombre))
+		{
+			nombre = prompt("Error;Ingrese nombre");
+		}
+
+		tipo = prompt("ingrese tipo de carta :monstruo, magica, trampa");
+
+		while(tipo != "monstruo" && tipo != "magica" && tipo != "trampa")
+		{
+			tipo = prompt("Error;ingrese tipo de carta :monstruo, magica, trampa");
+		}
+
+		rareza = prompt("Ingrese rareza : rara, super rara, ultra rara");
+
+		while(rareza != "rara" && rareza != "super rara" && rareza != "ultra rara")
+		{
+			rareza = prompt("Error;Ingrese rareza : rara, super rara, ultra rara");
+		}
+
+		precio = prompt("Ingrese precio");
+		precio = parseInt(precio);
+
+		while(isNaN(precio)|| precio <1)
+		{
+			precio = prompt("Error;Ingrese precio");
+			precio = parseInt(precio);
+		}
+
+		if(precio > precioMaximo || banderaDelPrimerMaximo == true)
+		{
+			precioMaximo = precio;
+			nombreMayorPrecio = nombre;
+			rarezaMayorPrecio = rareza;
+			banderaDelPrimerMaximo = false;
+		}
+
+		if(tipo == "trampa" && rareza =="rara"|| rareza =="super rara")
+		{
+			contadorTrampaRaraSuper = contadorTrampaRaraSuper + 1;
+		}
+		if(tipo =="trampa")
+		{
+			if(rareza =="")
+		}
+
+		if(tipo == "monstruo"&& rareza == "ultra rara" && precio < 500)
+		{
+			contadorMonstruoUltra = contadorMonstruoUltra + 1;
+			precioMonstruo = precioMonstruo + precio;
+		}
+
+		respuesta = confirm("Desea continuar??");
+	}
+
+	document.write("El nombre de la carta con mayor precio es : "+nombreMayorPrecio+" y es de rareza : "+rarezaMayorPrecio+ " <br>");
+	document.write("la cantidad de cartas trampa de rareza rara o super rara es : "+contadorTrampaRaraSuper+ " <br>");
+	
+	if(contadorMonstruoUltra !=0)
+	{
+		promedioPrecio = precioMonstruo / contadorMonstruoUltra;
+		document.write("El promedio de cartas monstruo con precio menor a 500 es : "+promedioPrecio+ " <br>");
+	}
+	else
+	{
+		document.write("Sin cartas solicitado");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 6-InstruccionFor/jsIteracionesFor-08.js
+++ 6-InstruccionFor/jsIteracionesFor-08.js
@@ -1,7 +1,181 @@
+/*
+EJERCICIO 1
+Para una veterinaria se necesita un programa que permita ingresar datos de perros con su precio de vacunación a pagar hasta que el cliente quiera. 
+Por cada perro, se ingresa:
+* raza: (validar "sharpei", "galgo", "pastor").
+* nombre,
+* edad (entre 1 y 25),
+* peso (mas de 0),
+* precio de consulta (desde 500 hasta 1500).
+
+Se pide informar por alert:
+a)El importe total a pagar por las vacunaciones, sin descuento.
+b)El importe total a pagar con descuento (solo si corresponde)
+    Si se vacunan más de 2 perros, se obtiene un 20% de descuento sobre el total a pagar.
+    Si se vacunan más de 4 perros, se obtiene un 30% de descuento sobre el total a pagar.
+c)La raza con más cantidad de perros
+d)Nombre, raza y edad del perro más viejo ingresado
+
+*/
+
 function mostrar()
 {
+	var raza;
+	var nombre;
+	var edad;
+	var peso;
+	var precioConsulta;
+	var respuesta;
+
+	//a
+	
+	var importeTotal;
+	var importeDesc;
+	var descuento;
+
+	//c
+	var contadorSharpei;
+	var contadorGalgo;
+	var contadorPastor;
+
+	//d
+	var nombrePerroViejo;
+	var razaPerroViejo;
+	var edadMaxima;
+	var banderaDelPrimerMaximo;
 
 
-	alert("ok");
+	respuesta = true;
+	contadorPastor = 0;
+	contadorGalgo = 0;
+	contadorSharpei = 0;
+	banderaDelPrimerMaximo = true;
+	importeTotal = 0;
+
+	while(respuesta == true)
+	{
+
+		raza = prompt("Ingrese raza :sharpei, galgo, pastor ");
+
+		while(raza != "sharpei" && raza != "galgo" && raza != "pastor")
+		{
+			raza = prompt("Error;Ingrese raza :sharpei, galgo, pastor ");
+		}
+
+		nombre = prompt("ingrese nombre");
+
+		while(!isNaN(nombre))
+		{
+			nombre = prompt("Error;ingrese nombre ");
+		}
+
+		edad = prompt("Ingrese edad");
+		edad = parseInt(edad);
+
+		while(isNaN(edad)|| edad < 1 || edad >25)
+		{
+			edad = prompt("Error;Ingrese edad");
+			edad = parseInt(edad);
+		}
+
+		peso = prompt("ingrese peso ");
+		peso = parseFloat(peso);
+
+		while(isNaN(peso) || peso < 1)
+		{
+			peso = prompt("error;ingrese peso ");
+			peso = parseFloat(peso);
+		}
+
+		precioConsulta = prompt("Ingrese precio");
+		precioConsulta = parseFloat(precioConsulta);
+
+		while(isNaN(precioConsulta)|| precioConsulta < 500 || precioConsulta >1500)
+		{
+			precioConsulta = prompt("Error;Ingrese precio");
+			precioConsulta = parseFloat(precioConsulta);
+		}
+
+		switch(raza)
+		{
+			case "sharpei":
+				contadorSharpei = contadorSharpei + 1;
+			break;
+			case "pastor":
+				contadorPastor = contadorPastor + 1;
+			break;
+			case "galgo":
+				contadorGalgo = contadorGalgo + 1;
+			break;
+		}
+
+		if(edad > edadMaxima || banderaDelPrimerMaximo == true)
+		{
+			edadMaxima = edad;
+			nombrePerroViejo = nombre;
+			razaPerroViejo = raza;
+
+			banderaDelPrimerMaximo = false;
+		}
+
+		importeTotal = importeTotal + precioConsulta;
+
+
+
+
+		respuesta = confirm("Desea continuar?")
+	}
+
+	cantidadDePerros = contadorGalgo + contadorSharpei + contadorPastor;
+
+	document.write("El importe Total  sin descuento : "+ importeTotal+ " <br>");
+
+	if(cantidadDePerros > 2 || cantidadDePerros < 5)
+	{
+		descuento = 20;
+	}
+	else
+	{
+		if(cantidadDePerros > 4)
+		{
+			descuento = 30;
+		}
+		else
+		{
+			descuento = 0;
+		}
+	}
+	if(descuento !=0)
+	{
+		descuento = (importeTotal * descuento) /100;
+		importeDesc = importeTotal - descuento;
+		document.write("El importe con descuento : "+importeDesc+" <br>");
+	}
+	if(contadorPastor > contadorGalgo && contadorPastor > contadorSharpei)
+	{
+		document.write("el de mas cantidad es Pastor <br>");
+	}
+	else
+	{
+		if(contadorSharpei > contadorGalgo)
+		{
+			document.write("El de mas cantidad es sharpei <br>");
+		}
+		else
+		{
+			document.write("El de mas cantidad es galgo <br>");
+		}
+	}
+
+
+
+	document.write("El nombre del perro mas viejo : "+nombrePerroViejo+ " y raza : "+razaPerroViejo+ " edad "+edadMaxima+ " <br>");
+	
+
+
+
+
+
+	
 
 }//FIN DE LA FUNCIÓN
